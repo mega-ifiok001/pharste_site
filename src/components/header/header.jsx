dc4bd1de4fd1805ce1f4df43f18f85db9d4bdc2a
megaifiok_001 <ifiokaniebiet@gmail.com>
2025-04-01 22:51:30 +0100

    some comimit

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import './header.css';
-import Logo from '../../asset/logo.png';
-import Ear from '../../asset/ear.png';
+import Logo from '../../asset/logo-white.png';
+import menu_btn from '../../asset/menu-btn.png';
 import { Link } from 'react-router-dom';
 
 function Header() {
   return (
     <>
-      <header className="container d-flex align-items-center justify-content-center w-100%">
-        <nav className="d-flex align-items-center justify-content-between">
+      <header className="container desktop_menu_container w-100%">
+        <nav className="desktop_menu">
           
             < img className='logo' src={Logo} alt="Company Logo" />
           
@@ -20,14 +20,43 @@ function Header() {
                 <li><Link to="/about">About</Link></li>
                 <li><Link to="/help">Help</Link></li>
             </ul>
-          <div className="nav-end d-flex align-items-center justify-content-center">
-            <img src={Ear} alt="Ear Plug" />
+          <div className="nav-end d-flex align-items-center justify-content-end">
+            {/* <img src={Ear} alt="Ear Plug" /> */}
             <button>
               <a href="#">Get the App</a>
             </button>
           </div>
         </nav>
       </header>
+
+
+
+      
+        <header className="container-fluid mobile_menu_container">
+          <img src={Logo} className="logo"  alt="company logo" />
+           
+          <div className="dropdowns">
+            <button>
+            <span></span>
+            <span></span>
+            <span></span>
+            </button>
+            <ul>
+            <li><Link to="/">Home</Link></li>
+                <li><Link to="/features">Features</Link></li>
+                <li><Link to="/pricing">Pricing</Link></li>
+                <li><Link to="/blog">Blog</Link></li>
+                <li><Link to="/about">About</Link></li>
+                <li><Link to="/help">Help</Link></li>
+            </ul>
+          </div>
+        </header>
+      
+
+
+
+
+
     </>
   );
 }

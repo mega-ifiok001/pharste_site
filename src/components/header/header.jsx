25fac9ad81b963ecb1fda2a01e9fc3de0bc16aca
megaifiok_001 <ifiokaniebiet@gmail.com>
2025-04-01 23:38:43 +0100

    done with nav

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import {useState} from 'react';
 import './header.css';
 import Logo from '../../asset/logo-white.png';
 import menu_btn from '../../asset/menu-btn.png';
 import { Link } from 'react-router-dom';
 
 function Header() {
+
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
   return (
     <>
       <header className="container desktop_menu_container w-100%">
@@ -36,12 +42,16 @@ function Header() {
           <img src={Logo} className="logo"  alt="company logo" />
            
           <div className="dropdowns">
-            <button>
+            <button className="menu-btn"  onClick={toggleMenu}>
             <span></span>
             <span></span>
             <span></span>
             </button>
-            <ul>
+            <ul  className={menuOpen ? 'open' : ''}>
+            <button className="cross-button"  onClick={toggleMenu}> 
+      <span></span>
+      <span></span>
+    </button>
             <li><Link to="/">Home</Link></li>
                 <li><Link to="/features">Features</Link></li>
                 <li><Link to="/pricing">Pricing</Link></li>

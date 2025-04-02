9839e5f2fc2130ff9191bb7d8b1d1521aa02e257
megaifiok_001 <ifiokaniebiet@gmail.com>
2025-04-02 10:22:13 +0100

    getter

--- src/components/hero-section/hero.jsx
+++ src/components/hero-section/hero.jsx
@@ -2,6 +2,11 @@ import React, { useEffect } from 'react';
 import './hero.css';
 import heroBg from '../../asset/Vector.png';
 import badge from '../../asset/Background.png';
+import apple from '../../asset/apple.png';
+import google from '../../asset/google.png';
+import stack from '../../asset/Container.png';
+import phone from '../../asset/iphone.png';
+import blur from '../../asset/blur-gradient.png';
 
 function Hero() {
   
@@ -22,6 +27,28 @@ function Hero() {
                      and fearless <br/> transactions, no matter where they are 🌐🌍
                     </p>
                 </div>
+
+                <div className="d-flex downloader">
+                    <img src={apple} alt="" />
+                    <img src={google} alt="" />
+                </div>
+
+                <div class="image-stack">
+        <img src={stack} alt="Image 1"/>
+        <div className="stack-text">
+            <span>150K+</span> <br/>
+            <span>Review</span>
+        </div>
+
+
+      
+    </div>
+
+
+    <div className="hero-bottom">
+          <img src={blur} className="blur" alt="" />
+          <img src={phone} className="phone" alt="" />
+        </div>
              </div>
             </div>
         </>
--- src/components/money/money.jsx
+++ src/components/money/money.jsx
@@ -1,163 +1,70 @@
 import React from 'react';
 import './money.css';
-import google from '../../asset/google.png'
-import apple from '../../asset/apple.png';
-import globe from '../../asset/globe.png';
-import dollar from '../../asset/dollar-lock.png';
-import safe_man from '../../asset/safe_man.png';
-import fees from '../../asset/Fees.png';
-import woman from '../../asset/woman.png';
-import bell from '../../asset/Notification.png';
-import bank from '../../asset/bank.png';
-import ScrollDep from '../../components/scroll_deb/scroll_deb';
-import Banner from '../banner/banner';
-import SliderComp from '../slider/slider';
-
+import trustBg from '../../asset/trusted.png';
+import fine__person from '../../asset/fine_person.png';
+import tab from '../../asset/tab_img.png';
+import protect from '../../asset/protect.png';
+import phone_bg from '../../asset/phone-bg.png';
 function Money(){
 
     return(
 
         <>
-          <div className="content text-start">
-          <h1>Make fast money moves</h1>
-         <p>The money app built for friends, families, and fearless transactions, no matter where they are 🌐🌍</p>
-          
-
-          <div className=" content-img">
-           <a href="#">
-           <img src={google} alt="google store" />
-           </a>
-
-           <a href="#">
-            <img src={apple} alt="apple store" />
-           </a>
-          </div>
+         <div className="trust_container mt-5">
+          <div className="trust-text">
+            <h1>Trusted by users across <br/>thirty countries</h1>
           </div>
 
-
-
-          {/* next section */}
-
-          <section className='section-def'>
-           <div className="header">
-           <h1 className="">
-            Why use pharste?
-            </h1>
-            <p>The money app built for friends, families, and fearless transactions, no matter where they are 🌐🌍</p>
-          
-           </div>
+          <div className="trust-bg container-lg">
+            <img src={trustBg} alt="" />
+          </div>
+         </div>
 
 
 
-          </section>
 
 
-          <section>
+         {/* why use pharste */}
 
-           <div className="send_money_tab">
-            <div className="tab_writeup">
-              <h1>
-              Send Money At The Speed Of Now
-              </h1>
-              <p>Transfer funds instantly to anyone, anywhere. What used to take days now happens in seconds. Whether sending across the room or across the globe, your money arrives right when you need it.</p>
+         <div className="why-use">
+          <div className="use-text ">
+            <h1>Why Use Pharste?</h1>
+
+            <div class=" mt-2 tab">
+            <div class="row ">
+            <div class="col-md-5">
+                <div class="content">
+                    <h2>Send Money At The <br/> Speed Of Now</h2>
+                    <p>Transfer funds instantly to anyone, anywhere. What used to take days now happens in seconds.</p>
+                    <img src={tab} className="tab-img" alt="Tab Image" />
+                </div>
             </div>
-                <img src={globe} alt="globe image" />
-             </div>
-
-
-             <div className="safety_tabs row">
-              <div className="send_money_tab col-md-8">
-                  <div className="safety_tab_writeup">
-                    <h1>
-                    Protection That Never Sleeps
-                    </h1>
-                    <p>
-                    Rest easy knowing your transactions are protected by military-grade encryption and advanced fraud monitoring. 
-                    </p>
-                  </div>
-
-                  <img src={dollar} alt="dollar lock" />
-              </div>
-
-              <div className="safety_tab_1 col-md-4">
-              <img src={safe_man} alt="" />
-              </div>
-             </div>
-
-
-
-             <div className="send_money_tab send_money_tab_2">
-            <div className=" tab_writeup_2 ">
-              <h1 className=""
-              style={{
-                color:'#A84D00',
-              }}
-              >
-             More For You, Less For Fees
-              </h1>
-              <p>Say goodbye to hidden charges and confusing fee structures. Our transparent pricing means you'll always know exactly what you're paying. </p>
+            <div class="col-md-7">
+                <img src={fine__person} class="img-fluid" alt="Responsive Image"/>
             </div>
-                <img 
-                src={fees}
-                style={{
-                  marginTop:'3rem',
-                  width:'30%'
-                }}
-                alt="globe image" />
-             </div>
+        </div>
+    </div>
 
 
 
 
-
-             <div className="watch_money row">
-             <div className="col-md-4 watch_money_tab">
-               <img src={woman} alt="woman image" />
-             </div>
-
-             <div className="send_money_tab_3  col-md-8 ">
-             <div className="tab_writeup_3 ">
-                <h1>Watch Your Money Move</h1>
-                <p>Never wonder where your money is again. Follow your transfers from start to finish with real-time
-                   tracking and instant notifications. </p>
-             </div>
-             <img src={bell}
-             style={{
-              width:'30%',
-
-             }} alt="bell" />
-             </div>
-             </div>
-
-
-
-
-             <div className="send_money_tab_4">
-             <div className="tab_writeup_4 ">
-                <h1>Grow Your Money Automatically</h1>
-                <p>Never wonder where your money is again. Follow your transfers from start to finish with real-time
-                   tracking and instant notifications. </p>
-             </div>
-             <img src={bank}
-             style={{
-              width:'30%',
-              
-             }} alt="bell" />
-             </div>
-          </section>
-
-
-          <section>
-            <ScrollDep/>
-          </section>
-
-          <section>
-            <Banner/>
-          </section>
-
-        <section>
-        <SliderComp/>
-        </section>
+    <div class=" mt-2 tab">
+            <div class="row ">
+            <div class="col-md-7">
+                <img src={protect} class="img-fluid" alt="Responsive Image"/>
+            </div>
+            <div class="col-md-5">
+                <div class="content">
+                    <h2>Send Money At The <br/> Speed Of Now</h2>
+                    <p>Transfer funds instantly to anyone, anywhere. What used to take days now happens in seconds.</p>
+                    <img src={phone_bg} className="tab-img" alt="Tab Image" />
+                </div>
+            </div>
+         
+        </div>
+    </div>
+          </div>
+         </div>
         </>
     )
 

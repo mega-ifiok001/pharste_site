fa6f936dda4861308afdf5c4edc887f3a6eaac68
mega-ifiok001 <ifiokaniebiet@gmaial.com>
2025-04-19 08:50:30 +0100

    gfds

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,24 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from '../src/pages/home';
 import About from './pages/About';
 import Features from './pages/features';
 import Help from './pages/help';
+import ScrollToTop from './components/scrollToTop/scrollToTop';
 import 'bootstrap/dist/css/bootstrap.min.css'; 
+
 function App() {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/features" element={<Features />} />
-                <Route path="/help" element={<Help />} />
-            </Routes>
-        </Router>
-    );
+  return (
+    <Router>
+      <ScrollToTop /> 
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/features" element={<Features />} />
+        <Route path="/help" element={<Help />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
